6324fdbf1e1bc56baf586b890fe0302a49cf1458
yaeun <nyeshinee0525@naver.com>
2024-02-01 13:14:55 +0900

    [feat]TILUI

---
+++ UMC-iOS/UMC-iOS/Presentation/ContentView/TIL/TILCell.swift
@@ -0,0 +1,18 @@
+//
+//  TILCell.swift
+//  UMC-iOS
+//
+//  Created by 나예은 on 2024/02/01.
+//
+
+import SwiftUI
+
+struct TILCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TILCell()
+}
---
+++ UMC-iOS/UMC-iOS/Presentation/ContentView/TIL/TILUI.swift
@@ -0,0 +1,18 @@
+//
+//  TILUI.swift
+//  UMC-iOS
+//
+//  Created by 나예은 on 2024/02/01.
+//
+
+import SwiftUI
+
+struct TILUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TILUI()
+}
