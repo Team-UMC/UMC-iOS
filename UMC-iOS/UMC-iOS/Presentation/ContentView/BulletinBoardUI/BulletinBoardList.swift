8db81ba5d8b2a9586d9c0135ad4c149bedb1eec4
yaeun <nyeshinee0525@naver.com>
2024-02-01 13:09:13 +0900

    [feat]BulletinBoardUI

---
+++ UMC-iOS/UMC-iOS/Presentation/ContentView/BulletinBoardUI/BulletinBoardList.swift
@@ -0,0 +1,34 @@
+//
+//  BulletinBoardList.swift
+//  BulletinBoardUI
+//
+//  Created by 나예은 on 2024/01/26.
+//
+
+import SwiftUI
+
+struct BulletinBoardList: View {
+    @State private var contentData: [Int] = Array(0..<10)
+
+    var body: some View {
+        List {
+            ForEach(contentData, id: \.self) { index in
+                UserContentPreList()
+                    .listRowInsets(EdgeInsets())
+                    .padding(.bottom,20)
+                    .padding(.top,20)
+                    
+            }
+        }
+        .listStyle(PlainListStyle())
+        .cornerRadius(12)
+    }
+}
+
+struct BulletinBoardList_Previews: PreviewProvider {
+    static var previews: some View {
+        BulletinBoardList()
+    }
+}
+
+
