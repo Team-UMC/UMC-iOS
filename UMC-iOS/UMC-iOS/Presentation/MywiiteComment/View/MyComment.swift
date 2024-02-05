db9ed7ab3ce833deb46f03c0506dd7a0d6e5a60c
yaeun <nyeshinee0525@naver.com>
2024-02-05 16:02:51 +0900

    [Refactor]게시판 검색

--- UMC-iOS/UMC-iOS/Presentation/MywiiteComment/View/MyComment.swift
+++ UMC-iOS/UMC-iOS/Presentation/MywiiteComment/View/MyComment.swift
@@ -1,18 +1,33 @@
 //
 //  MyComment.swift
-//  UMC-iOS
+//  BulletinBoard
 //
-//  Created by 나예은 on 2024/02/05.
+//  Created by 나예은 on 2024/02/01.
 //
 
 import SwiftUI
 
 struct MyComment: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        SearchBar(text: .constant(""), color: .constant("#F6F6F6"))
+            .padding(.leading,-50)
+            .padding(.trailing,-50)
+        
+        HStack{
+            //머지하고 연결
+            RadioButtonset(name: "게시글")
+            RadioButtonset(name: "댓글")
+            RadioButtonset(name: "좋아요 한 글")
+            Spacer()
+        }
+        .padding(.leading,240)
+        
+        MyContentList()
+            .padding(.top,-10)
     }
 }
 
 #Preview {
     MyComment()
 }
+
--- UMC-iOS/UMC-iOS/Presentation/MywiiteComment/View/MywriteSearch.swift
+++ UMC-iOS/UMC-iOS/Presentation/MywiiteComment/View/MywriteSearch.swift
@@ -1,18 +1,46 @@
 //
-//  MywriteSearch.swift
-//  UMC-iOS
+//  MyWriteSearch.swift
+//  BulletinBoard
 //
-//  Created by 나예은 on 2024/02/05.
+//  Created by 나예은 on 2024/02/01.
 //
 
 import SwiftUI
 
-struct MywriteSearch: View {
+struct MyWriteSearch: View {
+    @State private var searchText = ""
+    @State private var searchColor = "#F6F6F6"
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack{
+                SearchBar(text: $searchText, color: $searchColor)
+                    .padding(.top, 20)
+                    .onTapGesture {
+                        hideKeyboard()
+                    }
+                Button("취소") {
+                    print("취소버튼을 눌렀습니다")
+                }
+                .font(.system(size: 14))
+                .padding(.top, 20)
+                .padding(.leading,8)
+                .foregroundColor(Color("textColor"))
+                .background(.clear)
+            }
+            Spacer()
+            
+            MyContentList()
+                .padding(.top,-10)
+                .padding(.leading,15)
+                .padding(.trailing,0)
+        }
     }
 }
 
-#Preview {
-    MywriteSearch()
+struct MyWriteSearch_Previews: PreviewProvider {
+    static var previews: some View {
+        MyWriteSearch()
+    }
 }
+
+
