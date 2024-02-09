2fae84151a914b3c8bb436dcb310ba48b07cfe49
KyungsooLee <jmtkd9196@gmail.com>
2024-02-10 04:11:02 +0900

    feat: 투두리스트 뷰 Navigation 연결 및 임시 버튼 구현

--- UMC-iOS/UMC-iOS/Presentation/Home/View/ToDoListView.swift
+++ UMC-iOS/UMC-iOS/Presentation/Home/View/ToDoListView.swift
@@ -25,39 +25,7 @@ struct ToDoListView: View {
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 0) {
                     ForEach(0..<3) { index in
-                        ZStack {
-                            Rectangle()
-                                .frame(width: 122, height: 134)
-                                .foregroundColor(Color.tdlDisabled)
-                                .cornerRadius(20)
-                                .padding(.leading, 18)
-                            
-                            VStack(spacing: 0) {
-                                Button {
-                                    print("TDLPlusButton Clicked")
-                                } label: {
-                                    Image(systemName: "plus.circle.fill")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(width: 24, height: 24)
-                                        .foregroundColor(Color.tdlGray)
-                                        .offset(x: 8, y: 0)
-                                } // Button
-                                .padding(.bottom, 8)
-                                
-                                VStack { // ( )님의 투두를 기다리는 중이에요
-                                    HStack(spacing: 0) {
-                                        Text("원")
-                                            .fontWeight(.semibold)
-                                        Text("님의 투두를")
-                                    }
-                                    Text("기다리는 중이에요..😴")
-                                } // VStack
-                                .font(.system(size: 12))
-                                .kerning(-1.05)
-                                .padding(.leading, 18)
-                            } // VStack
-                        } // ZStack
+                        TodoEmptyCell()
                     } // ForEach
                 } // HStack
                 .padding(.trailing, 18)
@@ -66,4 +34,64 @@ struct ToDoListView: View {
             .padding(.top, 8)
         }
     }
+}
+
+struct TodoEmptyCell: View {
+    @State private var isClicked: Bool = false
+    var body: some View {
+        
+        
+        VStack(spacing: 0) {
+            Button {
+                print("TDLPlusButton Clicked")
+                isClicked.toggle()
+            } label: {
+                ZStack {
+                    Rectangle()
+                        .frame(width: 122, height: 134)
+                        .foregroundColor(Color.tdlDisabled)
+                        .cornerRadius(20)
+                        .padding(.leading, 18)
+                    VStack {
+                        Image(systemName: "plus.circle.fill")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 24, height: 24)
+                            .foregroundColor(Color.tdlGray)
+                            .offset(x: 8, y: 0)
+                        VStack { // ( )님의 투두를 기다리는 중이에요
+                            HStack(spacing: 0) {
+                                Text("원")
+                                    .fontWeight(.semibold)
+                                Text("님의 투두를")
+                            }
+                            Text("기다리는 중이에요..😴")
+                        } // VStack
+                        .font(.system(size: 12))
+                        .kerning(-1.05)
+                        .padding(.leading, 18)
+                        .foregroundStyle(Color.black)
+                    }
+                }
+            } // Button
+            .padding(.bottom, 8)
+            .navigationDestination(isPresented: $isClicked) {
+                ToDoListUI()
+                    .navigationTitle("To-Do List")
+                    .toolbar {
+                        ToolbarItem(placement: .topBarTrailing) {
+                            Button {
+                                print("완료 버튼 클릭")
+                            } label: {
+                                Text("완료")
+                            }
+                        }
+                    }
+            }
+        } // VStack
+    }
+}
+
+#Preview {
+    ToDoListView(TDLList: ["1", "2"])
 }
