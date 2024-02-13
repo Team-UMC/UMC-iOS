e1425c38c15db84c1fa3564beddfb07439048a2e
KyungsooLee <jmtkd9196@gmail.com>
2024-02-13 16:26:14 +0900

    feat: Board class 구현

--- UMC-iOS/UMC-iOS/Domain/Entities/Board.swift
+++ UMC-iOS/UMC-iOS/Domain/Entities/Board.swift
@@ -11,13 +11,36 @@ struct Board {
     typealias Identifier = String
     var id: Identifier = ""
     var writer: Member?
-    var title: String
-    var content: String
+    var title: String = ""
+    var content: String = ""
     var semesterPermission: [Semester]
     var hostType: HostType?
     var boardType: BoardType?
+    var hitCount: Int = 0
+    var heartCount: Int = 0
+    var commentCount: Int = 0
+    var isFixed: Bool = false
     
+    init(id: Identifier, writer: Member? = nil, title: String, content: String, semesterPermission: [Semester], hostType: HostType? = nil, boardType: BoardType? = nil, hitCount: Int, heartCount: Int, commentCount: Int, isFixed: Bool) {
+        self.id = id
+        self.writer = writer
+        self.title = title
+        self.content = content
+        self.semesterPermission = semesterPermission
+        self.hostType = hostType
+        self.boardType = boardType
+        self.hitCount = hitCount
+        self.heartCount = heartCount
+        self.commentCount = commentCount
+        self.isFixed = isFixed
+    }
     
+    init() {
+        self.writer = Member()
+        self.semesterPermission = []
+        self.hostType = .none
+        self.boardType = .none
+    }
 
 }
 
