8d95d5453156143b505c5d490729518586da8dc0
yaeun <nyeshinee0525@naver.com>
2024-02-15 12:53:51 +0900

    REFACTOR: UserProfileSocialAdd 수정

--- UMC-iOS/UMC-iOS/Presentation/UserProfileEditView/View/UserProfileSocialAdd.swift
+++ UMC-iOS/UMC-iOS/Presentation/UserProfileEditView/View/UserProfileSocialAdd.swift
@@ -26,7 +26,7 @@ struct UserProfileSocialAdd: View {
                     AbleStack()    // 기술
                     UMCSeason()    // 기수
                 }
-                SocialButtonSet(SocialimageName: socialImageName)  // 친구 추가 및 쪽지 보내기
+                SocialButtonSet()  // 친구 추가 및 쪽지 보내기
                 UserMessage()  // 상태 메세지 및 버튼
             }
             .padding(.bottom, 60)
