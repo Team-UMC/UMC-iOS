2b1c26e986ef9660bed3c8ab512d122ddeabc2b3
won <andrew112517@naver.com>
2024-02-15 13:39:49 +0900

    [Feat]Mascot뷰 배경, 음식 사진 빼고 구현

--- UMC-iOS/UMC-iOS/Presentation/GrowMascot/View/GrowMascotView.swift
+++ UMC-iOS/UMC-iOS/Presentation/GrowMascot/View/GrowMascotView.swift
@@ -12,9 +12,30 @@ struct GrowMascotView: View {
         ZStack {
             
             // 배경
-            Color.black.ignoresSafeArea(.all)
+            Color.gray.ignoresSafeArea(.all)
             
+            // 지구 이미지, 마스코트
             VStack(spacing: 0) {
+                
+                Spacer()
+                
+                ZStack() {
+                    
+                    Image("mascotEarthImage")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 342, height: 342)
+                        .padding(.bottom, -170)
+                    
+                    MascotImageView(mascotImageName: "mascotULevel1Image")
+                        .offset(y: -80)
+                    
+                } // ZStack
+                
+            } // HStack
+            
+            
+            VStack(spacing: 61) {
                 
                 
                 HStack(spacing: 0) {
@@ -23,12 +44,30 @@ struct GrowMascotView: View {
                     
                     Spacer()
                     
-                    UniversityRankButtonView()
-                    MyContributionButtonView()
+                    HStack(spacing: 10) {
+                        
+                        UniversityRankButtonView()
+                        MyContributionButtonView()
+                        
+                    } // HStack
                     
                 } // HStack
                 
+                // 마스코트 먹이
+                VStack(spacing: 12) {
+                    
+                    MascotFoodView(exp: 1, foodInfo: "은하수를 살짝 얹은 커스터드 푸딩")
+                    MascotFoodView(exp: 5, foodInfo: "별빛 스프링클이 가득한 블랙홀 도넛")
+                    MascotFoodView(exp: 10, foodInfo: "태양이 물든 선샤인 샤베트 아이스크림 ")
+                    MascotFoodView(exp: 30, foodInfo: "찬란한 별들이 쏙쏙 박힌 우주맛 롤케이크")
+                    
+                } // VStack
+                
+                Spacer()
+                
             } // VStack
+            .padding(.vertical, 20)
+            .padding(.horizontal, 22.5)
             
         } // ZStack
     }
---
+++ UMC-iOS/UMC-iOS/Presentation/GrowMascot/View/MascotFoodView.swift
@@ -0,0 +1,71 @@
+//
+//  MascotFoodView.swift
+//  UMC-iOS
+//
+//  Created by 김승원 on 2/15/24.
+//
+
+import SwiftUI
+
+struct MascotFoodView: View {
+    
+    var foodImageName: String = ""
+    var exp: Int = 0
+    var foodInfo: String = ""
+    var mascot: String = ""
+    
+    
+    var body: some View {
+        
+        Button {
+            print("\(foodInfo) Button Tapped. (\(exp) exp)")
+        } label: {
+            
+            HStack(spacing: 0) {
+                
+                // 먹이 이미지와 exp
+                VStack(spacing: 0) {
+                    
+                    Circle()
+                        .frame(width: 48, height: 48)
+                        .foregroundColor(.white)
+                    
+                    Text("\(exp)" + " EXP")
+                        .font(.system(size: 8))
+                        .fontWeight(.regular)
+                        .foregroundColor(.black)
+                        .padding(.vertical, 2)
+                        .padding(.horizontal, 8)
+                        .background(.white)
+                        .cornerRadius(12)
+                        .padding(.top, -6)
+                    
+                } // VStack
+                
+                // 먹이 설명
+                Text("\(foodInfo)")
+                    .font(.system(size: 12))
+                    .fontWeight(.regular)
+                    .kerning(-0.24)
+                    .foregroundColor(.white)
+                    .padding(.vertical, 8)
+                    .padding(.trailing, 12)
+                    .padding(.leading, 20)
+                    .background(Color.white.opacity(0.25))
+                    .cornerRadius(16)
+                    .padding(.leading, -12)
+                    .padding(.top, -6)
+                    .shadow(color: .white, radius: 18)
+                
+                Spacer()
+                
+            } // HStack
+            
+        } // Button
+        
+    }
+}
+
+#Preview {
+    MascotFoodView()
+}
